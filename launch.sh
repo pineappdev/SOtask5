6dd7e8193c6f87c13752a6b18a985342741c817e
Jacek <jc394143@students.mimuw.edu.pl>
2019-06-07 15:33:35 +0200

    added launch with extra disk

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-sudo qemu-system-x86_64 -curses -drive file=minix.img -enable-kvm -localtime -net user,hostfwd=tcp::10022-:22 -net nic,model=virtio -m 1024M
+sudo qemu-system-x86_64 -curses -drive file=minix.img -drive file=extra.img,format=raw,index=1,media=disk -enable-kvm -localtime -net user,hostfwd=tcp::10022-:22 -net nic,model=virtio -m 1024M
